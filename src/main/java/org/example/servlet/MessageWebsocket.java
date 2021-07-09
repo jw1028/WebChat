4ca6acf2fbab0dbd2ff1a95b822416f8fd1d3d41
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-09 13:31:55 +0800

    Update MessageWebsocket.java

--- src/main/java/org/example/servlet/MessageWebsocket.java
+++ src/main/java/org/example/servlet/MessageWebsocket.java
@@ -1,4 +1,5 @@
 package org.example.servlet;
+package org.example.servlet;
 
 import org.example.dao.MessageDAO;
 import org.example.dao.UserDAO;
@@ -10,55 +11,70 @@ import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.util.Date;
 import java.util.List;
 
-@ServerEndpoint("/message/{userId}")
-public class MessageWebsocket {
-
+/**
+ * WebSocket类
+ */
+@ServerEndpoint("/messageWebsocket/{userId}")
+public class MessageWebSocket {
+    
+    /**
+     *建立连接
+     */
     @OnOpen
-    public void onOpen(@PathParam("userId") Integer userId,
-                       Session session) throws IOException {
+    public void onOpen(@PathParam("userId") Integer userId, Session session) throws IOException {
         //1.把每个客户端的session都保存起来，之后转发消息到所有客户端要用
-        MessageCenter.addOnlineUser(userId, session);
+        MessageCenter.getInstance().addOnlineUser(userId, session);
         //2.查询本客户端（用户）上次登录前的消息（数据库查）
-         List<Message> list = MessageDAO.queryByLastLogout(userId);
+        List<Message> list = MessageDAO.queryByLastLogout(userId);
         //3.发送当前用户在上次登录后的消息
-        for(Message m : list) {
-            session.getBasicRemote().sendText(Util.serialize(m));
+        for(Message message : list){
+            session.getBasicRemote().sendText(Util.serialize(message));
         }
         System.out.println("建立连接："+userId);
     }
 
+    /**
+     * 服务端接收到客户端消息的响应事件
+     *发送消息
+     */
     @OnMessage
-    public void onMessage(Session session,
-                          String message){
+    public void onMessage(Session session, String message){
         //1.遍历保存的所有session，每个都发送消息
-        MessageCenter.sendMessage(message);
-        //2.消息还要保存在数据库
-        // 反序列化json字符串为message字符串
+//        MessageCenter.sendMessage(message);
+        MessageCenter messageCenter = MessageCenter.getInstance();
+        messageCenter.addMessage(message);
+        messageCenter.sendMessage();
+        //2.消息还要保存在数据库：
+        // （1）反序列化json字符串为message对象
         Message msg = Util.deserialize(message, Message.class);
-        //message设置接收消息的时间
-        msg.setSendTime(new Date());
-        //插入数据库
+        // （2）插入数据库
         int n = MessageDAO.insert(msg);
-        System.out.printf("接收到消息：%s\n", message);
+        System.out.println("接收到消息:" + message);
     }
 
+    /**
+     * 关闭连接
+     */
     @OnClose
     public void onClose(@PathParam("userId") Integer userId){
         //1.本客户端关闭连接，要在之前保存的session集合中，删除
-        MessageCenter.delOnlineUser(userId);
-        //2.建立连接
+        MessageCenter.getInstance().delOnlineUser(userId);
+        //2.建立连接要获取用户上次登录以后的消息，所以关闭长连接就是代表用户退出
+        //更新用户的上次登录时间
         int n = UserDAO.updateLastLogout(userId);
         System.out.println("关闭连接");
     }
-
+    /**
+     *异常处理
+     */
     @OnError
-    //出了异常才会执行
-    public void onError(Throwable t){
+    public void onError(@PathParam("userId") Integer userId, Throwable throwable){
         System.out.println("出错了");
-        t.printStackTrace();
-        //和关闭连接的操作一样
+        //和关闭连接的操作一样(删除当前的userId即可)
+        MessageCenter.getInstance().delOnlineUser(userId);
+        throwable.printStackTrace();
+
     }
 }
